3092f87c310f72d79a100d052a5a312262c98e43
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-01-30 18:03:20 +0100

    Use mamba/conda instead of pip

--- {{cookiecutter.project_slug}}/mlops_run.sh
+++ {{cookiecutter.project_slug}}/mlops_run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
 mkdir -p mlflow_artifacts
 mkdir -p logs
-chmod 777 mlflow_artifacts
-chmod 777 logs
+
+chmod -R 777 mlflow_artifacts
+chmod -R 777 logs
+
+jupyter lab --ip=0.0.0.0 --port=8893 > logs/jupyter.log 2>&1 &
+jupyter_pid=$!
+docker compose build
 docker compose up
+
