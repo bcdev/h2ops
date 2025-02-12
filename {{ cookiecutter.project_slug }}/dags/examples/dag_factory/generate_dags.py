6f4aa53e6b86015b4c3f03e434a5408d3973f37d
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-02-12 09:48:49 +0100

    update generate_dags.py

--- {{ cookiecutter.project_slug }}/dags/examples/dag_factory/generate_dags.py
+++ {{ cookiecutter.project_slug }}/dags/examples/dag_factory/generate_dags.py
@@ -9,17 +9,7 @@ print(f"Loading config file from: {config_file}")
 if not config_file.exists():
     raise FileNotFoundError(f"Config file not found: {config_file}")
 
-
-def print_xcom(ti):
-    bash_output = ti.xcom_pull(task_ids="task_1")  # Pull from task_1
-    print(f"Bash Output: {bash_output}")
-
-
 dag_factory = dagfactory.DagFactory(str(config_file))
 
 dag_factory.clean_dags(globals())
 dag_factory.generate_dags(globals())
-
-with DAG("basic_example_dag") as dag:
-    task_4 = dag.get_task("task_4")
-    task_4.python_callable = print_xcom
