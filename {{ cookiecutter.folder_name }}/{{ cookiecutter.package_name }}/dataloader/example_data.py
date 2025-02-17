3bbaecb16d7a765fec3b1db177b224c2a3ebdbd8
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-02-17 17:35:55 +0100

    more updates

--- {{ cookiecutter.folder_name }}/{{ cookiecutter.package_name }}/dataloader/example_data.py
+++ {{ cookiecutter.folder_name }}/{{ cookiecutter.package_name }}/dataloader/example_data.py
@@ -3,27 +3,22 @@ import os
 import numpy as np
 
 from {{ cookiecutter.package_name }}.utils.utils import (
-    get_latest_data_path,
     get_s3_client,
 )
 
-def load_data():
+def load_data(preprocessed_path, bucket_name):
     s3 = get_s3_client(
         endpoint_url=os.getenv("MLFLOW_S3_ENDPOINT_URL"),
         access_key=os.getenv("AWS_ACCESS_KEY_ID"),
         secret_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     )
 
-    bucket_name = "mnist-data"
-    base_folder = "preprocessing"
-    s3_path, filename = get_latest_data_path(
-        s3, bucket_name=bucket_name, base_folder=base_folder
-    )
     local_path = "/tmp"
-    local_file = f"{local_path}/{filename}"
-    s3.download_file(bucket_name, s3_path, local_file)
+    local_file = f"{local_path}/{preprocessed_path.split("/")[-1]}"
+    s3.download_file(bucket_name, preprocessed_path, local_file)
 
     data = np.load(local_file)
     X_train, y_train = data["X_train"], data["y_train"]
     X_test, y_test = data["X_test"], data["y_test"]
-    return (X_train, y_train), (X_test, y_test), s3_path
+    return (X_train, y_train), (X_test, y_test), preprocessed_path
+
